f6a6d9285409e3baae7115c389511db36c281d73
Danilo Pianini <danilo.pianini@unibo.it>
2025-01-30 10:52:35 +0100

    ci: disable npm publication in release

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,5 @@
 const publishCmd = `
 ./gradlew -PstagingRepositoryId=\${process.env.STAGING_REPO_ID} releaseStagingRepositoryOnMavenCentral || exit 3
-./gradlew publishJsPackageToNpmjsRegistry || exit 4
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
